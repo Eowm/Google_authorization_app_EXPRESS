89bf0f93ac318bd823a57cdb0ad77296baf2ecbe
Adam Nelip <Adam.Nelip@gmail.com>
2018-08-04 13:00:59 +0200

    Added repo

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '351396136540-0ub3j4untifr26qfumq2om0o5fgpgssh.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'gZx2LxUzqTu1bHP2nopJP0E0',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
